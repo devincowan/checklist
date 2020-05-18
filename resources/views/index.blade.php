e6cbc399277d145d06c8b6d7aae8190ca1030446
devincowan <dcquickdraw@gmail.com>
2020-05-18 10:59:48 -0600

    add template

---
+++ resources/views/index.blade.php
@@ -0,0 +1,45 @@
+<html>
+	<head>
+		<title>TODO</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="stylesheet" href="assets/css/todo.css" />
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header" class="alt">
+				<div class="inner">
+					<h1>TODO LIST</h1>
+					<p>JS AJAXING demo using a <a href="https://templated.co">TEMPLATED</a> view</p>
+				</div>
+			</header>
+
+			<!-- Wrapper -->
+			<div id="wrapper">
+
+				<section id="cta" class="main special">
+					<ul class="actions">
+						<a href="assets/js/todo.js" class="button big">View todo.js</a>
+						<a href="assets/js/main.js" class="button big">View main.js</a>
+					</ul>
+				</section>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<p class="copyright">&copy; Design: <a href="https://templated.co">TEMPLATED</a>. Images: <a href="https://unsplash.com">Unsplash</a>.</p>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+			<script src="assets/js/todo.js"></script>
+
+	</body>
+</html>
